f65ccf785a2857ae505562a9a68348d17e4e3a7a
Tatsam <tatsamshrestha93@gmail.com>
2024-09-30 11:26:14 -0700

    Datatable added

---
+++ DisasterManagement/Controllers/VictimManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DisasterManagement.Controllers
+{
+	public class VictimManagementController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
